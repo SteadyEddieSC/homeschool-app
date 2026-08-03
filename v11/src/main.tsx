d9d4a6a45ed023633a882403932b0b0c0aecdc46
Eddie Gugino <eddie.gugino@coalfire.com>
2026-08-03 19:42:47 -0400

    style(beta2): load synchronization and recovery styles

--- v11/src/main.tsx
+++ v11/src/main.tsx
@@ -4,6 +4,7 @@ import BetaApp from './BetaApp';
 import './styles.css';
 import './styles-alpha2.css';
 import './styles-beta1.css';
+import './styles-beta2.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Application root was not found.');
